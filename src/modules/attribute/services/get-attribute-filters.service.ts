251be882b3229e220d7b7e1bfed47a90b16e42c9
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2026-09-17 20:32:12 +0000

    filters

--- src/modules/attribute/services/get-attribute-filters.service.ts
+++ src/modules/attribute/services/get-attribute-filters.service.ts
@@ -3,8 +3,12 @@ import { getAttributesByType } from "@/modules/attribute/services";
 import type { AttributeFilters } from "@/modules/attribute/types";
 
 export async function getAttributeFilters(): Promise<AttributeFilters[]> {
+  const attributeTypes = ATTRIBUTE_TYPES_LIST.filter(
+    ({ code }) => code !== "characterStatus" && code !== "universeType",
+  );
+
   return Promise.all(
-    ATTRIBUTE_TYPES_LIST.map(async (attributeType) => {
+    attributeTypes.map(async (attributeType) => {
       const options = await getAttributesByType(attributeType.code);
 
       return {
